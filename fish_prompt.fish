296d5fb2a5cc75af23e7968d1a3ee010b28f4c49
Freddie Wang <wangyung@gmail.com>
2022-05-03 10:27:33 +0900

    Update fish_prompt for fish 3.4+

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,6 +3,19 @@
 # You can override some default options in your config.fish:
 #   set -g theme_display_git_untracked no
 
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  set -l show_untracked (git config --bool bash.showUntrackedFiles)
+  set untracked ''
+  if [ "$theme_display_git_untracked" = 'no' -o "$show_untracked" = 'false' ]
+    set untracked '--untracked-files=no'
+  end
+  echo (command git status -s --ignore-submodules=dirty $untracked 2>/dev/null)
+end
+
 set -x LSCOLORS "Exgxcxdxbxegedabagacad"
 
 function fish_prompt
@@ -22,11 +35,11 @@ function fish_prompt
   end
   set -l cwd $yellow(prompt_pwd)
 
-  if [ (git_branch_name) ]
-    set -l git_branch $green(git_branch_name)
+  if [ (_git_branch_name) ]
+    set -l git_branch $green(_git_branch_name)
     set git_info "$red [$git_branch$red]"
 
-    if git_is_touched
+    if [ (_is_git_dirty) ]
       set -l dirty "$red ✗"
       set git_info "$git_info$dirty"
     end
